7ee11c9b31877135e55c524857d74e175cfe3527
anaveed <ahsan_naveed@sfu.ca>
2019-06-20 15:49:21 -0700

    Change main

---
+++ console-app/console-app/UriBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace UriTests {
+
+    public class TestUriProgram {
+        public static Uri GetSimpleUri () {
+            var builder = new UriBuilder {
+                Scheme = "http",
+                Host = "packt.com"
+            };
+            return builder.Uri;
+        }
+
+        public static void Main () {
+            var simpleUri = GetSimpleUri();
+
+            Console.WriteLine(simpleUri.ToString());
+            Thread.Sleep(10000);
+        }
+    }
+
+}
