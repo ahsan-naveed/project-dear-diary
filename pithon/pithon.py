b17c1f4b085e1d1beba7e6330efeae8227b6f14d
ahsan-naveed <ahsannaveed007@yahoo.com>
2019-11-02 15:39:53 -0700

    Bill those gates

--- pithon/pithon.py
+++ pithon/pithon.py
@@ -14,6 +14,24 @@ def lcm(x, y):
    lcm = (x*y)//gcd(x,y)
    return lcm
 
+"""
+Fraction Class Implementing:
+    __init__
+    __str__
+    __add__
+    __mul__
+    __truediv__
+    __sub__
+    __radd__
+    __iadd__
+    __repr__
+    show
+    __gt__
+    __lt__
+    __le__
+    __eq__
+    __ne__
+"""
 class Fraction:
     def __init__(self, up,down):
         common = gcd(up, down)
@@ -80,15 +98,103 @@ class Fraction:
         return Fraction.__add__(self, value)
 
     def __iadd__(self, value):
-        return Fraction.__add__(self, value)
+        self.num = ((self.num * value.den) + (value.num * self.den))
+        self.den = self.den * value.den
+
+        return Fraction(self.num, self.den)
 
     def __repr__(self):
         return 'Fraction(' + str(self.num) + '/' + str(self.den) + ')'
 
+#######################################################################
+#######################################################################
+#######################################################################
 
+"""
+LogicGate Class
+"""
+class LogicGate:
+    def __init__(self, n):
+        self.label = n
+        self.output = None
 
-if __name__ == "__main__":
-    frac1 = Fraction(2, 2)
-    frac2 = Fraction(1, 2)
+    def getLabel(self):
+        return self.label
+
+    def getOutput(self):
+        self.output = self.performGateLogic()
+        return self.output
+
+"""
+BinaryGate Class Extends LogicGate
+"""
+class BinaryGate(LogicGate):
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def getPinA(self):
+        return (int(input("Enter Pin A input for gate " + self.getLabel() + "-->")))
     
-    print(frac1 + 1)
+    def getPinB(self):
+        return (int(input("Enter Pin B input for gate " + self.getLabel() + "-->")))
+
+"""
+UnaryGate Class Extends LogicGate
+"""
+class UnaryGate(LogicGate):
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pin = None
+
+    def getPin(self):
+        return (int(input("Enter Pin input for gate" + self.getLabel() + "-->")))
+
+"""
+AndGate class Extends BinaryGate
+"""
+class AndGate(BinaryGate):
+    def __init__(self, n):
+        super(AndGate, self).__init__(n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+"""
+OrGate class Extends BinaryGate
+"""
+class OrGate(BinaryGate):
+    def __init__(self, n):
+        super(OrGate, self).__init__(n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+
+class NotGate(UnaryGate):
+    def __init__(self, n):
+        super(NotGate, self).__init__(n)
+
+    def performGateLogic(self):
+        if self.getPin() == 1:
+            return 0
+        else:
+            return 1
+    
+if __name__ == "__main__":
+    g1 = AndGate("G1")
+    print(g1.getOutput())
