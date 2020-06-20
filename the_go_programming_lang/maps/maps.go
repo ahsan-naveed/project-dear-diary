6384f0f2fab1790474164342dbc3525c5eb9c09b
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-06-19 21:59:58 -0600

    Add maps

--- the_go_programming_lang/maps/maps.go
+++ the_go_programming_lang/maps/maps.go
@@ -92,8 +92,7 @@ func dedup() {
 // using slices as keys
 var m = make(map[string]int)
 
-func k(list []string) string { return fmt.Sprintf("%q", list) }
-
+func k(list []string) string  { return fmt.Sprintf("%q", list) }
 func Add(list []string)       { m[k(list)]++ }
 func Count(list []string) int { return m[k(list)] }
 
