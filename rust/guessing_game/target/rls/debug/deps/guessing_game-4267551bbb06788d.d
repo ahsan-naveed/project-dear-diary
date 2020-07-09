b9d6b4c5fa5d2367f56920ecf405481f96c01b59
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-07-09 00:04:44 -0600

    remove garbage

--- rust/guessing_game/target/rls/debug/deps/guessing_game-4267551bbb06788d.d
+++
@@ -1,5 +0,0 @@
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/rls/debug/deps/guessing_game-4267551bbb06788d.rmeta: src/main.rs
-
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/rls/debug/deps/guessing_game-4267551bbb06788d.d: src/main.rs
-
-src/main.rs:
--- rust/guessing_game/target/rls/debug/deps/guessing_game-e8cf3eb275374728.d
+++
@@ -1,5 +0,0 @@
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/rls/debug/deps/guessing_game-e8cf3eb275374728.rmeta: src/main.rs
-
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/rls/debug/deps/guessing_game-e8cf3eb275374728.d: src/main.rs
-
-src/main.rs:
