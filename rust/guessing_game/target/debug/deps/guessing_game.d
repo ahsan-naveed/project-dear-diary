2a5e7f92b02a8150a5772850a1dac96a9f7dac92
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-07-08 23:57:54 -0600

    Remove target

--- rust/guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/debug/deps/guessing_game: src/main.rs
-
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/debug/deps/guessing_game.d: src/main.rs
-
-src/main.rs:
--- rust/guessing_game/target/debug/deps/rand-3e0f427918adbaaa.d
+++
@@ -1,9 +0,0 @@
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/debug/deps/rand-3e0f427918adbaaa.rmeta: /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/debug/deps/librand-3e0f427918adbaaa.rlib: /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/ahsannaveed/Documents/tutorial-projects/rust/guessing_game/target/debug/deps/rand-3e0f427918adbaaa.d: /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
-/Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
-/Users/ahsannaveed/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
