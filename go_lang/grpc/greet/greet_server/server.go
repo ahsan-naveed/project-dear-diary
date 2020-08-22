f4776bfb189a9fc1540c18b6c11262eaa79932bd
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-08-21 19:52:58 -0600

    Add infra

--- go_lang/grpc/greet/greet_server/server.go
+++ go_lang/grpc/greet/greet_server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/ahsan-naveed/grpc/greet/greetpb"
 	"google.golang.org/grpc"
 )
 
@@ -12,11 +13,15 @@ type server struct{}
 
 func main() {
 	fmt.Println("Hello world")
-	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051") // default grpc port
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server: %v", err)
+	}
 }
