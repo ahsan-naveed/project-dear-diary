4a1be32ea9d2c8efdbbdf5592edef30398d6462e
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-09-28 21:45:09 -0600

    Clean imports

--- java/fp_java/code/extra/project-euler/problem_22/NameScores.java
+++ java/fp_java/code/extra/project-euler/problem_22/NameScores.java
@@ -1,9 +1,9 @@
-import java.util.*;
 import java.util.Collections;
-import java.io.*;
-import java.nio.file.*;
-import java.util.stream.IntStream;
-import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
 
 public class NameScroes {
     
