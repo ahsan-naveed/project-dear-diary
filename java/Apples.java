52166398b7098816d48b15350f8b42b8634ee2fe
ahsan-naveed <ahsannaveed007@yahoo.com>
2020-10-11 14:37:51 -0600

    Add prettyprintapple interface

--- java/Apples.java
+++ java/Apples.java
@@ -11,6 +11,40 @@ public class Apples {
 
     // program to an interface not an implementation
 
+    static public interface PrettyPrint {
+        String print(Apple apple);
+    }
+
+    static public class PrintWeight implements PrettyPrint {
+        public String print(Apple apple) {
+            return "Weight: " + apple.getWeight().toString();
+        }
+    }
+
+    static public class PrintColorAndWeight implements PrettyPrint {
+        public String print(Apple apple) {
+            StringBuilder result = new StringBuilder();
+            String color = Color.GREEN.equals(apple.getColor()) ? "Green" : "Red";
+            
+            result.append(color + " apple with " + apple.getWeight().toString() + " weight");
+
+            return result.toString();
+        }
+    }
+
+
+    static public class PrintHeavyLightColorApple implements PrettyPrint {
+        public String print(Apple apple) {
+            StringBuilder result = new StringBuilder();
+            String color = Color.GREEN.equals(apple.getColor()) ? "green" : "red";
+            String weightCategory = apple.getWeight() > 150 ? "Heavy" : "Light";
+            
+            result.append(weightCategory + " " + color + " " + "apple");
+
+            return result.toString();
+        }
+    }
+
     // class of algorithms (strategies)
     static public interface ApplePredicate {
         boolean test(Apple apple);
@@ -72,6 +106,12 @@ public class Apples {
             .collect(Collectors.toList());
     }
 
+    static public String prettyPrintApple(List<Apple> apples, PrettyPrint p) {
+        StringBuilder result = new StringBuilder(); 
+        apples.forEach(a -> result.append(p.print(a) + "\n"));
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         
         List<Apple> apples = new ArrayList<>();
@@ -92,9 +132,18 @@ public class Apples {
         System.out.println(filterApples(apples, new AppleHeavyWeightPredicate()));
         System.out.println(filterApples(apples, new AppleRedAndHeavyPredicate()));
 
+        System.out.println(prettyPrintApple(apples, new PrintWeight()));
+        System.out.println(prettyPrintApple(apples, new PrintColorAndWeight()));
+        System.out.println(prettyPrintApple(apples, new PrintHeavyLightColorApple()));
+
         // Note:
         // the behavior of the filterApples method depends on the code we pass to it
         // via the ApplePredicate object. We've parameterized the behavior of the
         // filterApples method
+
+        // by using lambdas, you can directly pass the expression RED.equals(apple.
+        // getColor()) && apple.getWeight() > 150 to the filterApples method without 
+        // having to define multiple ApplePredicate classes. This removes unnecessary 
+        // verbosity.
     }   
 }
