d65f33957aaa42d009bf35d358da3763c6d7e6e2
ahsan-naveed <ahsannaveed007@yahoo.com>
2021-01-03 18:58:13 -0800

    Stream.of() and Stream.empty()

--- java/BuildingStreams.java
+++ java/BuildingStreams.java
@@ -2,9 +2,9 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * NumericRanges
+ * BuildingStreams
  */
-public class NumericRanges {
+public class BuildingStreams {
 
     public static void main(String[] args) {
         Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
@@ -17,5 +17,23 @@ public class NumericRanges {
                         .mapToObj(b -> new double[] { a, b, Math.sqrt(a * a + b * b) }).filter(t -> t[2] % 1 == 0));
 
         pythagoreanTriplesV2.limit(5).forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        // Stream from values
+        Stream<String> stream = Stream.of("Modern", "Java", "in", "Action");
+        stream.map(String::toUpperCase).forEach(System.out::println);
+
+        // empty stream: Stream<String> emptyStream = Stream.empty();
+
+        // java 8
+        String homeValue = System.getProperty("home");
+        Stream<String> homeValueStream
+            = homeValue == null ? Stream.empty() : Stream.of(homeValue);
+        homeValueStream.forEach(System.out::println);
+
+        // java 9
+        Stream.ofNullable(System.getProperty("home")).forEach(System.out::println);
+
+        Stream<String> values = Stream.of("config", "home", "user").flatMap(key -> Stream.ofNullable(System.getProperty(key)));
+        values.forEach(System.out::println);
     }
 }
